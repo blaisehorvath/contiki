2beb364082f1b6ecabd3feab110dec4340a30e27
blaisehorvath <blaisehorvath@gmail.com>
2016-10-07 10:52:22 +0200

    Added a shell script that installs necessary things

---
+++ setup_1310.sh
@@ -0,0 +1,5 @@
+# This script helps setting up Contiki os development for TI Launchpad 1310 under apt based linux destros
+# downloading necessary packages
+
+git submodule update --init
+sudo apt-get install srecord 
