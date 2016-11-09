60ee7b5bf9993c0653d0295b30353d5b85bf6e06
Viktor Vaczi <viktor.vaczi.90@gmail.com>
2016-11-09 17:25:10 +0100

    new files for sensor system

---
+++ examples/ipv6/rpl-udp-true2air/bus_manager.h
@@ -0,0 +1,63 @@
+/*
+ * bus_manager.h
+ *
+ *  Created on: Nov 9, 2016
+ *      Author: v
+ */
+
+#ifndef EXAMPLES_IPV6_RPL_UDP_TRUE2AIR_BUS_MANAGER_H_
+#define EXAMPLES_IPV6_RPL_UDP_TRUE2AIR_BUS_MANAGER_H_
+/*! bus_comm_t describes a data type which is returned both from the
+ * i2c bus and to the spgbz as a data value from a sensor
+ * err is the flag which is true if some kind of error occured during the transmission
+ * data is the data returned
+ */
+typedef struct bus_comm_t{
+	uint8_t err;
+	double data;
+};
+/*!
+ * I2C_list_t is one element of the linked of I2C modules
+ * dev_id holds the dev_id which is burned into the sensor firmware, and used to identify the sensor
+ * addr is the address where the sensor can be accessed
+ */
+typedef struct I2C_list_t{
+	uint32_t dev_id;
+	uint8_t addr;
+	I2C_list_t NEXT;
+};
+/*!
+ * sensor_descriptor_t is a structure which holds all the parameters needed to initialize a sensor
+ */
+typedef struct sensor_descriptor_t{
+	bus_comm_t (*read) (int32_t,char );
+	bus_comm_t (*write)(int32_t,char, double);
+	char name[23];
+	uint32_t dev_id;
+	uint8_t sensor_num;
+};
+/*!
+ * i2c_read function is the function which is called when
+ * one tries to access a sensor which is attached to the i2c bus
+ * The slave knows the difference between read or write from the number of written bytes
+ * Node	|														| Sensor
+ * 		|----------W,addr|uint32_t dev_id|uin8_t sensor_id----->|
+ * 		|<---------------R,addr|bus_comm_t data|--------------->| Master request, slave answer
+ */
+bus_comm_t i2c_read(uint32_t dev_id, uint8_t sensor_id);
+/*!
+ * i2c_write function is the function which is called when
+ * one tries to access a sensor which is attached to the i2c bus
+ * The slave knows the difference between read or write from the number of written bytes
+ * Node	|														| Sensor
+ * 		|--W,addr|uint32_t dev_id|uin8_t sensor_id,double data->|
+ * 		|<---------------R,addr|bus_comm_t data|--------------->| Master request, slave answer
+ */
+bus_comm_t i2c_write(uint32_t dev_id, uint8_t sensor_id,double data);
+/*!
+ * The bus_check function checks if there's any new or disconnected sensors and
+ * calls the add or remove function from the spgbz accordingly
+ */
+void bus_check(void);
+
+#endif /* EXAMPLES_IPV6_RPL_UDP_TRUE2AIR_BUS_MANAGER_H_ */
