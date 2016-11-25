fd54d3569a227171838f21f28d0a825d489faab5
blaisehorvath <blaisehorvath@gmail.com>
2016-11-25 15:54:34 +0100

    tru2air is ready with msp_sensor controller

--- examples/saul/sensor_controller/sensor_controller.ino
+++ examples/saul/sensor_controller/sensor_controller.ino
@@ -1,22 +1,18 @@
 #include <Wire.h>
+//#include <SoftwareSerial.h>
 
-const int led = 13;
+//SoftwareSerial mySerial(9, 8); // RX, TX
+#define led P1_0
 
 void setup() {
-  Serial.print("Init\n");
-  pinMode(led, OUTPUT);     
+  
+  pinMode(led, OUTPUT);   
+    Wire.setModule(0);
   Wire.begin();        // join i2c bus (address optional for master)
-  Serial.begin(9600);  // start serial for output
-  toggleLed(led);
 
-/*
-  Wire.requestFrom(8, 6);    // request 6 bytes from slave device #8
+  //mySerial.begin(9600);  // start mySerial for output
+//mySerial.print("Init\n");
 
-  while (Wire.available()) { // slave may send less than requested
-    char c = Wire.read(); // receive a byte as character
-    Serial.print(c);         // print the character
-  }
-*/
   byte sendData[4] = {0xde, 0xad, 0xbe, 0xef};
   Wire.beginTransmission(8);
   Wire.write(sendData, 4);
@@ -25,17 +21,10 @@ void setup() {
   Wire.requestFrom(8, 1);
   while (Wire.available()) { // slave may send less than requested
     char c = Wire.read(); // receive a byte as character
-    Serial.print(c);         // print the character
+    //mySerial.print(c);         // print the character
   }
 
 }
 
 void loop() {
-  delay(500);
-}
-
-void toggleLed(int led){
-  digitalWrite(led,HIGH);
-  delay(50);  
-  digitalWrite(led,LOW);
 }
