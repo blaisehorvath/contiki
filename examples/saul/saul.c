cfe6bdc3f4e0dfd4aa0f619166756b21230c0db3
blaisehorvath <blaisehorvath@gmail.com>
2016-12-13 17:29:47 +0100

    Same issue but refactored to TI Lib functions..

--- examples/saul/saul.c
+++ examples/saul/saul.c
@@ -42,24 +42,46 @@ AUTOSTART_PROCESSES(&saul);
 
 
 void i2c_slave_data_isr () {
-	I2CSlaveIntClear(BOARD_I2C_INTERFACE_0, I2C_SLAVE_INT_DATA);
-	i2c_received_data = ti_lib_i2c_slave_data_get(BOARD_I2C_INTERFACE_0);
-	printf("int");
+	I2CSlaveIntClear(I2C0_BASE, I2C_SLAVE_INT_DATA);
+	(void)I2CSlaveDataGet;
+	;
 }
 
 PROCESS_THREAD(saul, ev, data)
 {
   PROCESS_BEGIN();
 
-  IntMasterEnable();
+  /* Initing the slave module */
+
+  /* First, make sure the SERIAL PD is on */
+  PRCMPowerDomainOn(PRCM_DOMAIN_SERIAL);
+  while((PRCMPowerDomainStatus(PRCM_DOMAIN_SERIAL)
+        != PRCM_DOMAIN_POWER_ON));
+
+  /* Enable the clock to I2C */
+  PRCMPeripheralRunEnable(PRCM_PERIPH_I2C0);
+  PRCMLoadSet();
+  while(!PRCMLoadGet());
+
+  /* Enable and initialize the I2C master module */
+  I2CSlaveInit(I2C0_BASE, slave_addr);
+
   printf("1");
-  I2CIntRegister(interface , i2c_slave_data_isr);
+  //------------------------------------------------------------------
+
+  IOCPinTypeI2c(I2C0_BASE, CC1310_IOID_SDA, CC1310_IOID_SCL);
+
+  //------------------------------------------------------------------
+
+  /* Setting up the interrupt */
+  I2CIntRegister(I2C0_BASE , i2c_slave_data_isr);
   printf("2");
-  board_i2c_select_slave(interface, slave_addr);
+
+  I2CSlaveIntEnable(I2C0_BASE, I2C_SLAVE_INT_DATA);
   printf("3");
-  I2CSlaveIntEnable(interface, I2C_SLAVE_INT_DATA);
-  printf("4");
+
   while(1);
+
   PROCESS_END();
 }
 /*---------------------------------------------------------------------------*/
@@ -68,9 +90,19 @@ PROCESS_THREAD(saul, ev, data)
 
 
 
-
-
-
+/*
+ *
+  IntMasterEnable();
+  printf("1");
+  I2CIntRegister(I2C0_BASE , i2c_slave_data_isr);
+  printf("2");
+  board_i2c_select_slave(interface, slave_addr);
+  printf("3");
+  I2CSlaveIntEnable(I2C0_BASE, I2C_SLAVE_INT_DATA);
+  printf("4");
+//  while(1);
+ *
+*/
 
 
 
@@ -85,3 +117,10 @@ PROCESS_THREAD(saul, ev, data)
 //  	while(++j < 1000000);
 //  	leds_off(LEDS_BLUE);
   //runTests();
+
+
+// from isr
+
+//	I2CSlaveIntClear(BOARD_I2C_INTERFACE_0, I2C_SLAVE_INT_DATA);
+//	i2c_received_data = ti_lib_i2c_slave_data_get(I2C0_BASE);
+//	printf("int");
