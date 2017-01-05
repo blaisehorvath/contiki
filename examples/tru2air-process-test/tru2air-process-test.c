bee80e4f19c769188ce994525afa24ddb78f67f1
blaisehorvath <blaisehorvath@gmail.com>
2017-01-05 10:29:53 +0100

    Experimenting with processes

--- examples/tru2air-process-test/tru2air-process-test.c
+++ examples/tru2air-process-test/tru2air-process-test.c
@@ -1,74 +1,78 @@
-  #include "contiki.h"
-  #include "dev/leds.h"
- 
-  #include <stdio.h> /* For printf() */
-  /*---------------------------------------------------------------------------*/
-  /* We declare the two processes */
-  PROCESS(hello_world_process, "Hello world process");
-  PROCESS(blink_process, "LED blink process");
- 
- /* We require the processes to be started automatically */
- AUTOSTART_PROCESSES(&hello_world_process, &blink_process);
- /*---------------------------------------------------------------------------*/
- /* Implementation of the first process */
- PROCESS_THREAD(hello_world_process, ev, data)
- {
-     // variables are declared static to ensure their values are kept
-     // between kernel calls.
-     static struct etimer timer;
-     static int count = 0;
+#include "contiki.h"
+#include "dev/leds.h"
 
-     // any process mustt start with this.
-     PROCESS_BEGIN();
+#include <stdio.h> /* For printf() */
 
-     // set the etimer module to generate an event in one second.
-     etimer_set(&timer, CLOCK_CONF_SECOND);
-     while (1)
-     {
-         // wait here for an event to happen
-         PROCESS_WAIT_EVENT();
+/* Variables the application specific event value */
+static process_event_t event_data_ready;
 
-         // if the event is the timer event as expected...
-         if(ev == PROCESS_EVENT_TIMER)
-         {
-             // do the process work
-             printf("Hello, world #%i\n", count);
-             count ++;
+/*---------------------------------------------------------------------------*/
+/* We declare the two processes */
+PROCESS(main_process, "Main process");
+PROCESS(alt_process, "Alt process");
 
-             // reset the timer so it will generate an other event
-             // the exact same time after it expired (periodicity guaranteed)
-             etimer_reset(&timer);
-         }
+/* We require the processes to be started automatically */
+AUTOSTART_PROCESSES(&main_process, &alt_process);
+/*---------------------------------------------------------------------------*/
+/* Implementation of the first process */
+PROCESS_THREAD(main_process, ev, data) {
+	// variables are declared static to ensure their values are kept
+	// between kernel calls.
+	static struct etimer timer;
+	static int count = 0;
 
-         // and loop
-     }
-     // any process must end with this, even if it is never reached.
-     PROCESS_END();
- }
- /*---------------------------------------------------------------------------*/
- /* Implementation of the second process */
- PROCESS_THREAD(blink_process, ev, data)
- {
-     static struct etimer timer;
-     static uint8_t leds_state = 0;
-     PROCESS_BEGIN();
+	// any process mustt start with this.
+	PROCESS_BEGIN();
 
-     while (1)
-     {
-         // we set the timer from here every time
-         etimer_set(&timer, CLOCK_CONF_SECOND / 4);
+	/* allocate the required event */
+	event_data_ready = process_alloc_event();
 
-         // and wait until the vent we receive is the one we're waiting for
-         PROCESS_WAIT_EVENT_UNTIL(ev == PROCESS_EVENT_TIMER);
+	/* Initialize the temperature sensor */
+	printf("Init main process\n");
 
-	 printf("From another process\n");
-         // update the LEDs
-	 /*
-         leds_off(0xFF);
-         leds_on(leds_state);
-         leds_state += 1;
-	 */
-     }
-     PROCESS_END();
- }
- /*---------------------------------------------------------------------------*/
+
+	// set the etimer module to generate an event in one second.
+	etimer_set(&timer, CLOCK_CONF_SECOND / 4);
+
+	while (1) {
+		// wait here for the timer to expire
+		PROCESS_WAIT_EVENT_UNTIL(ev == PROCESS_EVENT_TIMER);
+
+		leds_toggle(LEDS_RED);
+
+		count++;
+		printf("%d \n", count);
+
+		if (count % 4 == 0) {
+
+			// post an event to the print process
+			// and pass a pointer to the last measure as data
+			process_post(&alt_process, event_data_ready, &count);
+		}
+
+		// reset the timer so it will generate another event
+		etimer_reset(&timer);
+	}
+	// any process must end with this, even if it is never reached.
+PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
+/* Implementation of the second process */
+PROCESS_THREAD(alt_process, ev, data) {
+PROCESS_BEGIN();
+
+while (1) {
+	// wait until we get a data_ready event
+	PROCESS_WAIT_EVENT_UNTIL(ev == event_data_ready);
+	int green_led = 0;
+	leds_on(LEDS_GREEN);
+	while(++green_led<2000000000);
+	printf("%d \n", green_led);
+	leds_off(LEDS_GREEN);
+
+	// display it
+	printf("Alt process\n");
+}
+PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
