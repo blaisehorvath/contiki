673f7225082080eb3a2061d998e25610dec57127
Viktor Vaczi <viktor.vaczi.90@gmail.com>
2017-02-11 14:02:52 +0100

    Init works, refactoring....

--- examples/ipv6/rpl-udp-true2air/tru2air_i2c_protocol.c
+++ examples/ipv6/rpl-udp-true2air/tru2air_i2c_protocol.c
@@ -17,155 +17,279 @@
 
 
 /* Temporary Variables */
-extern void i2c_slave_data_isr (); //TODO: init_tru2air_snesor_node should require a function pointer to this instead of extern
+extern void i2c_slave_data_isr(); //TODO: init_tru2air_snesor_node should require a function pointer to this instead of extern
 
 /* Globals */
-volatile tru2air_sensor_node_t DEVICE = {0,0,0};
+volatile tru2air_sensor_node_t DEVICE = {0, 0, 0};
 volatile unsigned char STATE = I2C_SLAVE_LISTEN;
 volatile unsigned char ERROR = NO_I2C_ERROR;
 unsigned char currentSensor = 0; //TODO: reset on the proper place
 
 void printI2CPkt(i2c_pkt_t *pkt) {
-	int i = 0;
-	printf("pkt->dev_id: ");
-	printf("%d\n",pkt->dev_id);
-	printf("pkt->action: ");
-	printf("%d\n",pkt->action);
-	printf("pkt->error: ");
-	printf("%d\n",pkt->error);
-	for (i = 0; i < 32; i++) {
-		printf("pkt->data[");
-		printf("%d",i);
-		printf("]: ");
-		printf("%d\n",pkt->data[i]);
-	}
-	printf("pkt->CRC: ");
-	printf("%d\n",pkt->CRC);
+    int i = 0;
+    printf("pkt->dev_id: ");
+    printf("%d\n", pkt->dev_id);
+    printf("pkt->action: ");
+    printf("%d\n", pkt->action);
+    printf("pkt->error: ");
+    printf("%d\n", pkt->error);
+    for (i = 0; i < 32; i++) {
+        printf("pkt->data[");
+        printf("%d", i);
+        printf("]: ");
+        printf("%d\n", pkt->data[i]);
+    }
+    printf("pkt->CRC: ");
+    printf("%d\n", pkt->CRC);
 }
 
 
-uint16_t crc16(uint8_t* data_p, uint8_t length){
-	uint8_t x;
-	uint16_t crc = 0xFFFF;
+uint16_t crc16(uint8_t *data_p, uint8_t length) {
+    uint8_t x;
+    uint16_t crc = 0xFFFF;
 
-	while (length--){
-		x = crc >> 8 ^ *data_p++;
-		x ^= x>>4;
-		crc = (crc << 8) ^ ((uint16_t)(x << 12)) ^ ((uint16_t)(x <<5)) ^ ((uint16_t)x);
-	}
-	return crc;
+    while (length--) {
+        x = crc >> 8 ^ *data_p++;
+        x ^= x >> 4;
+        crc = (crc << 8) ^ ((uint16_t)(x << 12)) ^ ((uint16_t)(x << 5)) ^ ((uint16_t) x);
+    }
+    return crc;
 }
-void convertLEToBE4 (uint32_t* from, uint32_t* to) {
-	char i = 0;
-	while(i<4) {
-		((char*)to)[3-i] = *(((char*)from)+i);
-		i++;
-	}
+
+void convertLEToBE4(uint32_t *from, uint32_t *to) {
+    char i = 0;
+    while (i < 4) {
+        ((char *) to)[3 - i] = *(((char *) from) + i);
+        i++;
+    }
 }
-uint32_t convertLEToBE4Return (uint32_t* from) {
-	uint32_t retVal = 0;
-	char i = 0;
-	while(i<4) {
-		((char*)&retVal)[3-i] = *(((char*)from)+i);
-		i++;
-	}
-	return retVal;
+
+uint32_t convertLEToBE4Return(uint32_t *from) {
+    uint32_t retVal = 0;
+    char i = 0;
+    while (i < 4) {
+        ((char *) &retVal)[3 - i] = *(((char *) from) + i);
+        i++;
+    }
+    return retVal;
 }
-bool bus_manager_exchange_pkts(i2c_pkt_t* pkt_out, i2c_pkt_t* pkt_in,uint8_t i2c_addr){
-	i2c_pkt_t pktToSend = *pkt_out;
-	convertLEToBE4(&pkt_out->dev_id,&pktToSend.dev_id);
-	pktToSend.CRC = crc16(((uint8_t*)(&pktToSend)), sizeof(i2c_pkt_t)- sizeof(uint16_t));
-	board_i2c_select(BOARD_I2C_INTERFACE_0, i2c_addr);
-	if(!board_i2c_write(&pktToSend, sizeof(i2c_pkt_t))) return false;
-	if(!board_i2c_read(pkt_in, sizeof(i2c_pkt_t))) return false;
-	//convertLEToBE4(&temp,&pkt_in->dev_id); //TODO: Who will be responsible for le2be??CRC checking etc...
-	board_i2c_shutdown();
-	return true;
+
+bool bus_manager_exchange_pkts(i2c_pkt_t *pkt_out, i2c_pkt_t *pkt_in, uint8_t i2c_addr) {
+    i2c_pkt_t pktToSend = *pkt_out;
+    convertLEToBE4(&pkt_out->dev_id, &pktToSend.dev_id);
+    pktToSend.CRC = crc16(((uint8_t * )(&pktToSend)), sizeof(i2c_pkt_t) - sizeof(uint16_t));
+    //TODO:Fn should be called here which from action/sensor type gets the right information.
+
+    board_i2c_select(BOARD_I2C_INTERFACE_0, i2c_addr);
+    if (!board_i2c_write(&pktToSend, sizeof(i2c_pkt_t))) return false;// TODO:Possible wait here, less errors
+    if (!board_i2c_read(pkt_in, sizeof(i2c_pkt_t))) return false;
+    //convertLEToBE4(&temp,&pkt_in->dev_id); //TODO: Who will be responsible for le2be??CRC checking etc...
+    board_i2c_shutdown();
+    return true;
 }
+
 volatile int testSum = 0;
 volatile int testGood = 0;
-void i2c_bus_checker(){
-	//printf("in bus checker %d\n",STATE);
-	uint8_t sensactBuff, i,retvalread,retvalwrite;
-	for (i = 8; i< I2C_BUS_ADDRESS_RANGE; i++){
-		if(i2c_devices[i]){
-			//printf("%d device exsists \n",i);
-			board_i2c_select(BOARD_I2C_INTERFACE_0, i);
-			retvalwrite = board_i2c_write_single(GET_SENSACT_NUM);
-			sensactBuff = 0;
-			retvalread = board_i2c_read_single(&sensactBuff); // TODO: Check return value too.
-			if(!retvalwrite || (!sensactBuff && retvalread)){
-				//printf("%d device deleted \n",i);
-				sam_del_device(i2c_devices[i]);
-				i2c_devices[i] = 0;
-				// TODO: unregister device from SAM and i2c_devices
-			}
-			board_i2c_shutdown();
-		}
-	}
-	/* Register I2C Slave Interrupt */
-	bus_manager_register_i2c_isr(i2c_slave_data_isr);
-	/* Inititng I2C SLAVE as 0x25  and  enabling the registered I2C Slave Interrupt */
-	bus_manager_init_i2c_slave(0x25);
+
+void i2c_bus_checker() {
+    //printf("in bus checker %d\n",STATE);
+    uint8_t sensactBuff, i, retvalread, retvalwrite;
+    for (i = 8; i < I2C_BUS_ADDRESS_RANGE; i++) {
+        if (i2c_devices[i]) {
+            //printf("%d device exsists \n",i);
+            board_i2c_select(BOARD_I2C_INTERFACE_0, i);
+            retvalwrite = board_i2c_write_single(GET_SENSACT_NUM);
+            sensactBuff = 0;
+            retvalread = board_i2c_read_single(&sensactBuff); // TODO: Check return value too.
+            if (!retvalwrite || (!sensactBuff && retvalread)) {
+                //printf("%d device deleted \n",i);
+                sam_del_device(i2c_devices[i]);
+                i2c_devices[i] = 0;
+                // TODO: unregister device from SAM and i2c_devices
+            }
+            board_i2c_shutdown();
+        }
+    }
+    /* Register I2C Slave Interrupt */
+    bus_manager_register_i2c_isr(i2c_slave_data_isr);
+    /* Inititng I2C SLAVE as 0x25  and  enabling the registered I2C Slave Interrupt */
+    bus_manager_init_i2c_slave(0x25);
 }
-bool errorHandler(bool error, unsigned char errorType){
-	if(error){
-		STATE = I2C_ERROR;
-		ERROR = errorType;
-	}
-	return error;
+
+bool errorHandler(bool error, unsigned char errorType) {
+    if (error) {
+        STATE = I2C_ERROR;
+        ERROR = errorType;
+    }
+    return error;
 }
-void init_tru2air_sensor_node(){
-	bool tru2air_sensor_device_inited = false;
-	unsigned char headerBuff[2];
-	unsigned char nameBuff[23];
-	unsigned char typeBuff[2];
-	unsigned char sensactBuff;
-	i2c_pkt_t in_pkt = 		{0,0,0,{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},0};
-	i2c_pkt_t out_pkt = 	{0,0,0,{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},0};
-	/* Timing variables */
-	while (!tru2air_sensor_device_inited) {
-		switch (STATE) {
-			case (I2C_SLAVE_LISTEN):
 
-				/* Reseting DEVICE */
-				DEVICE = (tru2air_sensor_node_t){0,0,0};
+void init_tru2air_sensor_node() {
+    int i;
+    bool tru2air_sensor_device_inited = false;
+    unsigned char headerBuff[2];
+    unsigned char nameBuff[23];
+    unsigned char typeBuff[2];
+    unsigned char sensactBuff;
+    i2c_pkt_t in_pkt = {0, 0, 0,
+                        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                         0}, 0};
+    i2c_pkt_t out_pkt = {0, 0, 0,
+                         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                          0}, 0};
+    /* Timing variables */
+    while (!tru2air_sensor_device_inited) {
+        switch (STATE) {
+            case (I2C_SLAVE_LISTEN):
 
-				/* Register I2C Slave Interrupt */
-				bus_manager_register_i2c_isr(i2c_slave_data_isr);
+                /* Reseting DEVICE */
+                DEVICE = (tru2air_sensor_node_t) {0, 0, 0};
 
-				/* Inititng I2C SLAVE as 0x25  and  enabling the registered I2C Slave Interrupt */
-				bus_manager_init_i2c_slave(0x25);
+                /* Register I2C Slave Interrupt */
+                bus_manager_register_i2c_isr(i2c_slave_data_isr);
 
-				//printf("\n[STATE] -> NODE_I2C_SLAVE_LISTEN \n\n");
-				tru2air_sensor_device_inited = true;
-				break;
+                /* Inititng I2C SLAVE as 0x25  and  enabling the registered I2C Slave Interrupt */
+                bus_manager_init_i2c_slave(0x25);
 
-			case (NODE_I2C_MASTER_INIT):
-				//printf("[STATE] -> NODE_I2C_MASTER_INIT\n");
+                //printf("\n[STATE] -> NODE_I2C_SLAVE_LISTEN \n\n");
+                tru2air_sensor_device_inited = true;
+                break;
 
-				if (DEVICE.i2c_addr) {
-					I2CIntUnregister(I2C0_BASE);
-					bus_manager_disable_i2c_slave();
-					//printf("[INFO] disabling i2c slave \n");
-					out_pkt.action = GET_SENSACT_NUM;
-					out_pkt.data[0]=2;
-					out_pkt.data[1]=3;
-					out_pkt.dev_id= DEVICE.dev_addr;
-					out_pkt.error = 5;
-					//out_pkt.CRC = crc16(((uint8_t*)(&out_pkt)), sizeof(i2c_pkt_t)- sizeof(uint16_t));
-					testSum++;
-					if(!bus_manager_exchange_pkts(&out_pkt,&in_pkt,DEVICE.i2c_addr)) {printf("error in the bus somewhere!!!");board_i2c_shutdown(); STATE = I2C_SLAVE_LISTEN; break;}
-					if (in_pkt.CRC == crc16((uint8_t *)&in_pkt, sizeof(i2c_pkt_t)- sizeof(uint16_t)))
-						testGood++;
-					printf("testGood: %d, testSum:%d \n",testGood,testSum);
-					//printf("[INFO] tru2air sensor node: 0x%08x", convertLEToBE4Return(&in_pkt.dev_id));
-					//printI2CPkt(&in_pkt);
-					STATE = I2C_SLAVE_LISTEN;
-				}
-				break;
-		}
-	}
+            case (NODE_I2C_MASTER_INIT):
+                //printf("[STATE] -> NODE_I2C_MASTER_INIT\n");
+
+                if (DEVICE.i2c_addr) {
+                    I2CIntUnregister(I2C0_BASE);
+                    bus_manager_disable_i2c_slave();
+                    //printf("[INFO] disabling i2c slave \n");
+                    out_pkt.action = GET_SENSACT_NUM;
+                    out_pkt.dev_id = DEVICE.dev_addr;
+                    out_pkt.error = NO_I2C_ERROR;
+
+                    testSum++;
+                    if (!bus_manager_exchange_pkts(&out_pkt, &in_pkt, DEVICE.i2c_addr)) {
+                        //TODO: ERRORRRRR state
+                        printf("error in the bus somewhere!!!");
+                        board_i2c_shutdown();
+                        STATE = I2C_SLAVE_LISTEN;
+                        break;
+                    }
+                    if (in_pkt.CRC == crc16((uint8_t * ) & in_pkt, sizeof(i2c_pkt_t) - sizeof(uint16_t)))
+                        {
+                            testGood++;
+                            DEVICE.sensact_num = in_pkt.data[0];//TODO: Should be mapped with a struct both here and on arduino.
+                            currentSensor = 0;
+                            printf("[INFO] tru2air sensor node: 0x%08x has 0x%02x sensors\n\n", DEVICE.dev_addr,
+                                   DEVICE.sensact_num);
+                            //printI2CPkt(&in_pkt);
+                            STATE = REQUIRE_SENSACT_NAME;
+                        }
+                    else {
+                        STATE = NODE_I2C_MASTER_INIT;//TODO: So many todos...
+                    }
+                    printf("testGood: %d, testSum:%d \n", testGood, testSum);
+
+                }
+                break;
+            case (REQUIRE_SENSACT_NAME):
+                printf("[STATE] -> GET_SENSOR_NAME\n");
+                for  (i = 0; i< sizeof(i2c_pkt_t);i++) ((char*)(&out_pkt))[i] = 0;//zero out.
+                out_pkt.action = GET_SENSOR_NAME;
+                out_pkt.data[0] = currentSensor;
+
+                if (!bus_manager_exchange_pkts(&out_pkt, &in_pkt, DEVICE.i2c_addr)) {
+                    //TODO: ERRORRRRR state
+                    printf("error in the bus somewhere!!!");
+                    board_i2c_shutdown();
+                    STATE = I2C_SLAVE_LISTEN;
+                    break;
+                }
+                if (in_pkt.CRC == crc16((uint8_t * ) & in_pkt, sizeof(i2c_pkt_t) - sizeof(uint16_t)))
+                {
+                    strcpy(nameBuff, in_pkt.data);
+                    STATE = REQUIRE_SENSOR_RETURN_TYPE; //TODO: Continue from here
+                }
+                else {
+                    STATE = NODE_I2C_MASTER_INIT;//TODO: So many todos...
+                }
+                break;
+
+
+                
+            case (REQUIRE_SENSOR_RETURN_TYPE):
+                printf("[STATE] -> GET_SENSOR_TYPE\n");
+
+                for  (i = 0; i< sizeof(i2c_pkt_t);i++) ((char*)(&out_pkt))[i] = 0;//zero out.
+                out_pkt.action = GET_SENSOR_TYPE;
+                out_pkt.data[0] = currentSensor;
+
+                if (!bus_manager_exchange_pkts(&out_pkt, &in_pkt, DEVICE.i2c_addr)) {
+                    //TODO: ERRORRRRR state
+                    printf("error in the bus somewhere!!!");
+                    board_i2c_shutdown();
+                    STATE = I2C_SLAVE_LISTEN;
+                    break;
+                }
+                if (in_pkt.CRC == crc16((uint8_t * ) & in_pkt, sizeof(i2c_pkt_t) - sizeof(uint16_t)))
+                {
+                    typeBuff[0] = in_pkt.data[1];
+                    typeBuff[1] = in_pkt.data[0]; //TODO:BELE
+                    STATE = REQUIRE_SENSOR_RETURN_TYPE; //TODO: Continue from here
+                }
+                else {
+                    STATE = NODE_I2C_MASTER_INIT;//TODO: So many todos...
+                }
+
+                printf("[GOT SENSACT] Name: %s Type: 0x%04x \n", nameBuff, *(uint16_t *) typeBuff);
+                STATE = REGISTER_TO_SAM;
+                break;
+
+            case (REGISTER_TO_SAM):
+
+                printf("[STATE] -> REGISTER_TO_SAM\n");
+                sensact_descriptor_t new_sensact;
+
+                strcpy((char *) &(new_sensact.name), nameBuff);
+                new_sensact.dev_id = DEVICE.dev_addr;
+                new_sensact.sensact_id = currentSensor;
+                new_sensact.read = bus_manager_r_sensact;
+                new_sensact.write = bus_manager_w_sensact;
+                new_sensact.sensact_type = *((uint16_t *) typeBuff);
+
+                sam_add_sensact(new_sensact);
+
+                if (++currentSensor == DEVICE.sensact_num) {
+                    currentSensor = 0;
+                    STATE = I2C_SLAVE_LISTEN;
+                    printf("[I2C SENSACTS INIT SUCCESSFUL]\n");
+                } else {
+                    STATE = REQUIRE_SENSACT_NAME;
+                }
+
+                break;
+            case (I2C_ERROR):
+
+                tru2air_sensor_device_inited = false;
+
+                switch (ERROR) {
+                    case (I2C_SENSACT_NUM_NULL_ERROR):
+                        ERROR = NO_I2C_ERROR;
+                        STATE = I2C_SLAVE_LISTEN;
+                        printf("[ERROR] I2C SENSACT NUM NULL ERROR\n");
+                        break;
+
+                    case (I2C_SENSACT_NUM_ERROR):
+                        ERROR = NO_I2C_ERROR;
+                        STATE = I2C_SLAVE_LISTEN;
+                        printf("[ERROR] I2C SENSACT NUM ERROR\n");
+                        break;
+
+                    default:;
+                }
+
+                break;
+        }
+    }
 }
 
 
--- examples/saul/i2c_sensor_arduino/cc1310_i2c_initiated_sensor_slave/cc1310_i2c_initiated_sensor_slave.ino
+++ examples/saul/i2c_sensor_arduino/cc1310_i2c_initiated_sensor_slave/cc1310_i2c_initiated_sensor_slave.ino
@@ -94,7 +94,8 @@ unsigned int uint32_answer = 15;
 sensact_descriptor_t sensors[] = {{"TRU2AIR_RELAY", SENSACT_TRU2AIR_RELAY}};
 unsigned char device_addr[] = {0xde, 0xad, 0xbe, 0xef};
 i2c_pkt_t lastReceivedPkt;
-int resetVal= 0;
+int resetVal = 0;
+
 void setup() {
 
     Serial.begin(9600);  // start serial for output
@@ -128,20 +129,45 @@ void setup() {
 }
 
 void loop() {
-    if(resetVal) {resetVal = 0; delay(10);setup();};
+    //if(resetVal) {resetVal = 0; delay(10);setup();};
 }
 
 void receiveCb(int numBytes) {
     Serial.print("numbYtes:");
     Serial.println(numBytes);
-    i2c_pkt_t* pkt;
+    i2c_pkt_t *pkt;
     uint8_t receivedData[sizeof(i2c_pkt_t)];
     int i = 0;
     for (i = 0; i < numBytes; i++) receivedData[i] = Wire.read();
-    pkt = (i2c_pkt_t*)receivedData;
+    pkt = (i2c_pkt_t *) receivedData;
     printI2CPkt(pkt);
-    if (pkt->CRC == crc16((uint8_t *)pkt, sizeof(i2c_pkt_t)- sizeof(uint16_t))) Serial.println("Good pkt"); else Serial.println("Bad pkt"); //TODO: Check device ID TOO!!
+    if (pkt->CRC == crc16((uint8_t *) pkt, sizeof(i2c_pkt_t) - sizeof(uint16_t)))
+        Serial.println("Good pkt");
+    else Serial.println("Bad pkt"); //TODO: Check device ID TOO!!
     lastReceivedPkt = *pkt;
+    switch (pkt->action) {
+        case GET_SENSACT_NUM:
+            Serial.print("[STATE] -> GET_SENSACT_NUM \n");
+            STATE = GET_SENSACT_NUM;
+            break;
+        case GET_SENSOR_NAME:
+            Serial.print("[STATE] -> GET_SENSOR_NAME \n");
+            STATE = GET_SENSOR_NAME;
+            break;
+        case GET_SENSOR_TYPE:
+            Serial.print("[STATE] -> GET_SENSOR_TYPE");
+            STATE = GET_SENSOR_TYPE;
+            break;
+
+        case SENS_ACT_WRITE:
+            STATE = SENS_ACT_WRITE;
+            break;
+        case SENS_ACT_READ:
+            STATE = SENS_ACT_READ;
+        default:
+            Serial.print("[ERROR] Invalid state receieved! -> ");
+            break;
+    }
 }
 
 void requestCb() {
@@ -149,14 +175,77 @@ void requestCb() {
     i2c_pkt_t pkt;
     pkt = lastReceivedPkt;
     int i = 0;
-    for (i = 0 ; i < 32 ; i++) pkt.data[i]++;
-    pkt.action++;
-    pkt.CRC = crc16((uint8_t *)&pkt, sizeof(i2c_pkt_t)- sizeof(uint16_t));
-    Wire.write((uint8_t*)&pkt, sizeof(i2c_pkt_t));
-    Serial.print("[END OF REQUEST]\n");
-    resetVal = 1;
+//    for (i = 0; i < 32; i++) pkt.data[i]++;
+//    pkt.action++;
     //delay(200);
     //setup();
+
+    switch (STATE) {
+        case GET_SENSACT_NUM:
+            pkt.data[0] = sizeof(sensors) / sizeof(sensact_descriptor_t);
+            //Wire.write(sizeof(sensors) / sizeof(sensact_descriptor_t));
+            break;
+
+        case GET_SENSOR_NAME:
+            //TODO: handle bad HEADER
+            for(i = 0; i < sizeof(sensors[HEADER.specifier].name); i++) pkt.data[i] = sensors[lastReceivedPkt.data[0]].name[i];
+            break;
+
+        case GET_SENSOR_TYPE:
+            for(i = 0; i < 2; i++) pkt.data[i] = ((uint8_t*)( &sensors[lastReceivedPkt.data[0]].type))[i];
+            //Wire.write((char *) (&sensors[HEADER.specifier].type), 2);
+            break;
+        case SENS_ACT_WRITE:
+            break;
+        case SENS_ACT_READ:
+
+            float measurement;
+            char BEresultArr[4];
+            memset(SENSACT_MEASURE_RESULT, 0x00, SENSACT_DATA_SIZE);
+
+            switch (HEADER.specifier) {
+
+                /*
+
+                //pressure
+                case 0:
+                  measurement = bme.readPressure();
+                  convertLEFloatToBE(&measurement, BEresultArr);
+
+                  memcpy(SENSACT_MEASURE_RESULT, BEresultArr, sizeof(float));
+                  Wire.write((char*)&SENSACT_MEASURE_RESULT, SENSACT_DATA_SIZE);
+                  break;
+                //temp
+                case 1:
+                  measurement = bme.readTemperature();
+                  convertLEFloatToBE(&measurement, BEresultArr);
+
+                  memcpy(SENSACT_MEASURE_RESULT, BEresultArr, sizeof(float));
+                  Wire.write((char*)&SENSACT_MEASURE_RESULT, SENSACT_DATA_SIZE);
+                  break;
+                //hum
+                case 2:
+                  measurement = bme.readHumidity();
+                  convertLEFloatToBE(&measurement, BEresultArr);
+
+                  memcpy(SENSACT_MEASURE_RESULT, BEresultArr, sizeof(float));
+                  Wire.write((char*)&SENSACT_MEASURE_RESULT, SENSACT_DATA_SIZE);
+                  break;
+
+                default:
+                  ;
+                  */
+            }
+
+            break;
+
+        default:
+            break;
+    }
+    pkt.CRC = crc16((uint8_t * ) & pkt, sizeof(i2c_pkt_t) - sizeof(uint16_t));
+    Wire.write((uint8_t * ) & pkt, sizeof(i2c_pkt_t));
+    Serial.print("[END OF REQUEST]\n");
+    resetVal = 1;
 }
 
 void printHex4(byte *data) {
@@ -192,14 +281,14 @@ void printI2CPkt(i2c_pkt_t *pkt) {
     Serial.println(pkt->CRC);
 }
 
-uint16_t crc16(uint8_t* data_p, uint8_t length){
+uint16_t crc16(uint8_t *data_p, uint8_t length) {
     uint8_t x;
     uint16_t crc = 0xFFFF;
 
-    while (length--){
+    while (length--) {
         x = crc >> 8 ^ *data_p++;
-        x ^= x>>4;
-        crc = (crc << 8) ^ ((uint16_t)(x << 12)) ^ ((uint16_t)(x <<5)) ^ ((uint16_t)x);
+        x ^= x >> 4;
+        crc = (crc << 8) ^ ((uint16_t)(x << 12)) ^ ((uint16_t)(x << 5)) ^ ((uint16_t) x);
     }
     return crc;
 }
